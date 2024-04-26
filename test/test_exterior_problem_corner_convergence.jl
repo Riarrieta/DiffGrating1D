214838b23d1e9f26b62c495aefc70ce24bc5fa32
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2024-04-26 18:17:34 -0400

    add no regularization comparison

--- test/test_exterior_problem_corner_convergence.jl
+++ test/test_exterior_problem_corner_convergence.jl
@@ -16,6 +16,7 @@ sol_x0 = sol(x0)
 
 ## test u = D[ϕd]
 data = Dict()
+err_noregularization = Float64[]
 for N in Nlist
     for p in plist
         domain = DF.DomainWith1Corner(φ,k,N,p)
@@ -29,12 +30,24 @@ for N in Nlist
         error = abs((sol_x0-sol_approx)/sol_x0)
         data[(N,p)] = error
     end
+    # compare with no regularization
+    domain = DF.Domain(φ,k,N)
+    sol_φ = [sol(DF.point(q)) for q in domain.quad]
+    rhs = 2*sol_φ
+    # dpot
+    Zmatrix = DF.double_layer_matrix_plus_identity(domain)
+    ϕ = Zmatrix\rhs
+    Dpot = DF.double_layer_potential(domain,ϕ)
+    sol_approx = Dpot(x0)
+    error = abs((sol_x0-sol_approx)/sol_x0)
+    push!(err_noregularization,error)
 end
 
 ## Plots
-fig = plot(xaxis=:log,yaxis=:log)
+fig = plot(xaxis=:log,yaxis=:log,legend=:bottomleft)
 for p in plist
     errlist = [data[(N,p)] for N in Nlist]
     plot!(Nlist,errlist,label="p = $p")
 end
+plot!(Nlist,err_noregularization,label="no regularization",color=:black)
 plot(fig)
