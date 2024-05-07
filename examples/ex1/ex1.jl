cd7673256b125f9f1084e687c01ede7dbbe93eba
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2024-05-07 13:39:28 -0400

    Example 1 is working!

--- examples/ex1/ex1.jl
+++ examples/ex1/ex1.jl
@@ -4,7 +4,7 @@ using LinearAlgebra
 const DF = DiffGrating1D
 include("geometry.jl")
 
-geo = geometry(;homogeneous=true);
+geo = geometry(;homogeneous=false);
 #DF.check_geometry(geo;ϵtol=1e-4)
 #DF.check_homogeneous_geometry(geo;ϵtol=1e-4)
 
@@ -16,6 +16,6 @@ plot!(geo.domains[2],tangent=false,normal=false)
 u_reflected,r_coeff,u_transmitted,t_coeff = DF.solve_diffraction_problem(geo);
 
 # diffraction efficiencies
-_,β0 = DF.αβfactors(geo)
+_,β0 = DF.αβfactors(geo)  # β0 of incident wave
 r_eff = @. geo.βtop/β0*abs2(r_coeff)
-t_eff = @. (geo.kbottom^2*geo.βbottom)/(geo.ktop^2*β0)*abs2(t_coeff)
+t_eff = @. geo.βbottom/β0*abs2(t_coeff)   # (geo.kbottom^2*geo.βbottom)/(geo.ktop^2*β0)*abs2(t_coeff)
