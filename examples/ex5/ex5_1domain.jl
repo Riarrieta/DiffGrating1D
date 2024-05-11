77f9dc79e280a350a5ce51a7e7b96cbf8551aad2
Rodrigo Arrieta Candia <riarrieta@uc.cl>
2024-05-11 10:32:17 -0400

    ex5_1domain working

--- examples/ex5/ex5_1domain.jl
+++ examples/ex5/ex5_1domain.jl
@@ -7,7 +7,7 @@ include("geometry.jl")
 # First geometry
 wL = 0.5
 nlevels = 36
-geo = geometry(;wL,nlevels,homogeneous=false);
+geo = geometry(;wL,nlevels=1,homogeneous=false);
 #DF.check_geometry(geo;ϵtol=1e-4)
 #DF.check_homogeneous_geometry(geo;ϵtol=1e-4)
 
@@ -19,7 +19,8 @@ end
 plot(fig)
 
 ## Diffraction problem
-u_reflected,r_coeff,u_transmitted,t_coeff = DF.solve_diffraction_problem(geo);
+u_reflected,r_coeff,u_transmitted,t_coeff = solve_for_multiple_same_domain(geo;nlevels);
+
 
 # diffraction efficiencies
 _,β0 = DF.αβfactors(geo)  # β0 of incident wave
--- examples/ex5/geometry.jl
+++ examples/ex5/geometry.jl
@@ -43,3 +43,46 @@ function geometry(;nlevels=1,L=1,wL=0.35,homogeneous=false)
     geo = DF.Geometry(domains,kbottom,ktop,L,α0,Jmax)
     return geo
 end
+
+function solve_for_multiple_same_domain(geo::DF.Geometry;nlevels)
+    @assert length(geo.domains) == 1
+    @assert geo.domains[1] isa DF.DomainWithInclusion
+    domain = geo.domains[1]
+    # initial conditions
+    γ = DF.γfactor(geo)
+    α0,β0 = DF.αβfactors(geo)
+    Q = -geo.Gbottom  # -iB^(2)
+    Y = I(size(Q,1))
+    # domain N matrices
+    N11,N12,N21,N22 = DF.obtain_reduced_ntd_map(domain,γ)
+    # iterate
+    for (i) in 1:nlevels # from bottom to top
+        @info "Solving Domain $i"
+        # multiply by -1, since the normals of adyacent domains point in opposite directions
+        rmul!(Q,-one(ComplexF64))  
+        # obtain Q and Y from Domain
+        Z = (I-N11*Q) \ N12
+        Q = inv(N22+N21*Q*Z)
+        Y = Y*Z*Q
+    end
+    @info "Solving Top Domain"
+    # solve top boundary
+    topdom = DF.topdomain(geo)
+    top_points = (DF.point(DF.qpoint(topdom,i)) for i in DF.topboundary_indices(topdom))
+    L = geo.L
+    Δz = (nlevels-1)*L
+    u_inc(x) = exp(im*(α0*x[1]-β0*(x[2]+Δz)))   # incident planewave
+    u_incident = [u_inc(x) for x in top_points]   
+    β0 = geo.βtop[geo.Jmax+1]      # β of incident planewave
+    rhs = [-2*im*β0*u for u in u_incident]
+    Lmatrix = Q-geo.Gtop  # Q-iB^(1)
+    # solve and get fields
+    utop = Lmatrix\rhs
+    u_reflected = utop-u_incident
+    u_transmitted = Y*utop
+    # get reflection and transmission coeff
+    r_coeff = geo.Ttop*u_reflected
+    t_coeff = geo.Tbottom*u_transmitted
+    @info "Done"
+    return u_reflected,r_coeff,u_transmitted,t_coeff
+end
